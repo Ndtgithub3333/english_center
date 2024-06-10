7eca81d102ef462b2a8f9181c3aeede056197987
Thang Nguyen <nguyenducthangg30@gmail.com>
2024-06-10 09:24:03 +0700

    fetchData.js

---
+++ src/utils/fetchData.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+export const API_URL = 'http://192.168.110.106:5000';
+
+
+export const postApi = async(url, post, token ) => {
+   const res = await axios.post( `${API_URL}/api/${url}`, post, {      headers: { Authorization: token} 
+   })
+   return res;
+}
+
+export const getApi = async(url, token ) => {
+   const res = await axios.get(`${API_URL}/api/${url}`, {
+      headers: { Authorization: token}
+   })
+   return res;
+}
+
+export const patchApi = async(url, post, token ) => {
+   const res = await axios.patch( `${API_URL}/api/${url}`, post, {
+      headers: { Authorization: token}
+   })
+   return res;
+}
+
+export const putApi = async(url, post, token ) => {
+   const res = await axios.put( `${API_URL}/api/${url}`, post, {
+      headers: { Authorization: token}
+   })
+   return res;
+}
+
+export const delApi = async(url, token ) => {
+   const res = await axios.delete( `${API_URL}/api/${url}`, {
+      headers: { Authorization: token}
+   })
+   return res;
+}
